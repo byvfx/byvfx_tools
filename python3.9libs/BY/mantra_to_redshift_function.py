7f5d76b5107f25c2519f19d0236fa3e009ee2272
Brandon Young <brandon@byvfx.com>
2023-09-24 08:37:43 -0700

    updated python lib3.9 with updated functions

--- python3.9libs/BY/mantra_to_redshift_function.py
+++ python3.9libs/BY/mantra_to_redshift_function.py
@@ -1,3 +1,4 @@
+import hou
 def convert_mantra_to_redshift(selected_nodes=None):
     """
     Convert Mantra lights in Houdini to Redshift lights.
@@ -17,6 +18,7 @@ def convert_mantra_to_redshift(selected_nodes=None):
         "light_colorb": "light_colorb",
         "light_type": "light_type",
         "coneangle": "coneangle",
+        "areageometry": "RSL_meshObject",
     }
 
     # Mapping for the light modes from Mantra to Redshift
@@ -29,7 +31,7 @@ def convert_mantra_to_redshift(selected_nodes=None):
         5: 3,
         6: 3,
         7: 3,
-        8: 3,
+        8: 0,
     }
 
     if not selected_nodes:
@@ -46,13 +48,13 @@ def convert_mantra_to_redshift(selected_nodes=None):
         # Fetch node type
         main_node_type = node.type().name().split("::")[0]
 
-        print(f"Processing node {node.name()} of type {main_node_type}.")  # Debug statement
+        #print(f"Processing node {node.name()} of type {main_node_type}.")  # Debug statement
 
         # Determine the Redshift light type based on Mantra light type
         rs_light_type = "rslight" if main_node_type == "hlight" else "rslightdome" if main_node_type == "envlight" else None
 
         if rs_light_type:
-            print(f"Determined light type: {rs_light_type}.")  # Debug statement
+            #print(f"Determined light type: {rs_light_type}.")  # Debug statement
 
             # Cache parent node path to minimize repeated calls
             if not parent_path:
@@ -63,7 +65,7 @@ def convert_mantra_to_redshift(selected_nodes=None):
             # Create a new Redshift Light and inherit the name from the Mantra light
             rs_node = hou.node(parent_path).createNode(rs_light_type, node_name="CONVERTED_" + old_name)
             
-            print(f"Created new Redshift node: {rs_node.name()}.")  # Debug statement
+            #print(f"Created new Redshift node: {rs_node.name()}.")  # Debug statement
 
             # Set the new Redshift light's position and move it slightly
             rs_node.setPosition(node.position())
@@ -72,16 +74,26 @@ def convert_mantra_to_redshift(selected_nodes=None):
             # Transfer parameters according to the mapping
             for mantra_param, rs_param in param_mapping.items():
                 if node.parm(mantra_param) and rs_node.parm(rs_param):
-
+                     # Set RSL_areashape if light type is 3
+                        
                     # If dealing with light type, there's special handling
                     if mantra_param == "light_type":
                         mantra_value = node.parm(mantra_param).evalAsInt()  # Assuming light_type gives an integer value
-
+                    
+                        # Set RSL_areashape based on Mantra light type
+                        if mantra_value == 3:
+                            rs_node.parm("RSL_areaShape").set(1)
+                        elif mantra_value == 4:
+                            rs_node.parm("RSL_areaShape").set(2)
+                        elif mantra_value == 6:
+                            rs_node.parm("RSL_areaShape").set(4)
+                            rs_node.parm("RSL_meshObject").set(node.parm("areageometry").eval())
+                
                         # Check if the Mantra light has cone enabled
                         cone_parm = node.parm("coneenable")
                         cone_enabled = cone_parm.eval() if cone_parm else False
                         
-                        # Decide the RS value based on Mantra value and cone status
+                         # Decide the RS value based on Mantra value and cone status
                         if cone_enabled:
                             rs_value = 2  # Set RS value to 2 if cone is enabled
                         else:
@@ -98,9 +110,8 @@ def convert_mantra_to_redshift(selected_nodes=None):
                     # For other parameters, just map directly
                     else:
                         rs_node.parm(rs_param).set(node.parm(mantra_param).eval())
-            
-                else:
-                    print(f"Failed to map parameter: {mantra_param} to {rs_param}.")  # Debug statement
-
+                rs_node.setWorldTransform(node.worldTransform())
+                #else:
+                   # print(f"Failed to map parameter: {mantra_param} to {rs_param}.")  # Debug statement
         else:
             print(f"Could not determine the Redshift light type for {main_node_type}.")  # Debug statement

f7de0cc9806ed9b31377d49f31ca9af32825b8d2
Brandon Young <brandon@byvfx.com>
2025-02-01 19:46:10 -0800

    added a check an error check to let the user know there if there is no keyframes or animated nodes.

--- python3.11libs/byvfx/utils/color_anim_nodes.py
+++ python3.11libs/byvfx/utils/color_anim_nodes.py
@@ -2,18 +2,27 @@ import hou
 
 def color_animated_nodes():
     """Colorizes nodes with keyframes in the current network editor."""
-    user_color = hou.ui.selectColor()
-    if user_color is None:
-        return
-
+    
     network = hou.ui.paneTabOfType(hou.paneTabType.NetworkEditor)
     if not network or not network.currentNode():
         return
     
     parent = network.currentNode().parent()
 
+    animated_nodes = []
     for node in parent.children():
         for parm in node.parms():
             if parm.keyframes():  # Check for actual keyframes
-                node.setColor(user_color)
+                animated_nodes.append(node)
                 break
+
+    if not animated_nodes:
+        hou.ui.displayMessage("No animated nodes found")
+        return
+
+    user_color = hou.ui.selectColor()
+    if user_color is None:
+        return
+
+    for node in animated_nodes:
+        node.setColor(user_color)
